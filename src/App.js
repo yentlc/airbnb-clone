eb64aeb7970907e41784b32f057bb9a5df12d1d4
yentlc <98667887+yentlc@users.noreply.github.com>
2022-05-13 13:39:17 -0300

    added hero page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Heropage from './components/Heropage';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
+      <Heropage />
     </div>
   );
 }
---
+++ src/components/Heropage.js
@@ -0,0 +1,14 @@
+export default function Heropage() {
+  return (
+    <header className='heropage'>
+      <img className='hero-img' src='/images/hero-img.png' alt='' />
+      <div className='Titles'>
+        <h1>Online Experiences</h1>
+        <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </header>
+  );
+}
