c276cdd6faf4584e5a8e47c3a6ee6ede832c6fa6
yentlc <98667887+yentlc@users.noreply.github.com>
2022-05-13 21:43:37 -0300

    basic card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,8 @@
 export default function Card() {
   return (
     <section className='card'>
-      <h3>SOLD OUT</h3>
-      <img src='/images/katie-zaferes.png' />
+      <img src='/images/katie-zaferes.png' alt='katie zaferes' />
+      <h3 className='status'>SOLD OUT</h3>
       <div className='rating'>
         <i class='fa-solid fa-star'></i>
         <p className='rating'>5.0</p>
