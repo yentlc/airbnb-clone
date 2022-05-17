c6a4503b437dec6e0f280bbd7a7c93f3aabc4f09
yentlc <98667887+yentlc@users.noreply.github.com>
2022-05-17 17:12:22 -0300

    added the cards and some style

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import './App.css';
 import Card from './components/Card';
 import Heropage from './components/Heropage';
 import Navbar from './components/Navbar';
+import Data from './Data';
 
 function App() {
+  let cardData = Data.map((card) => {
+    return (
+      <Card
+        img={card.coverImg}
+        status={card.openSpots == 0 ? 'SOLD OUT' : 'AVAIABLE'}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        country={card.location}
+        title={card.title}
+        price={card.price}
+      />
+    );
+  });
   return (
     <div className='App'>
       <Navbar />
       <Heropage />
-      <Card
+      <section className='cards'>{cardData}</section>
+      {/* <Card
         img='katie-zaferes.png'
         status='SOLD OUT'
         rating='5.0'
@@ -16,7 +31,7 @@ function App() {
         country='USA'
         title='Life Lessons with Katie Zaferes'
         price='136'
-      />
+  /> */}
     </div>
   );
 }
